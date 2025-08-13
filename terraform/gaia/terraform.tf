aca7f2a77f44b216762630ff07ac649aee62b750
janpuc <janpuc@proton.me>
2025-08-13 20:13:46 +0200

    feat(gaia): Add flux

--- terraform/gaia/terraform.tf
+++ terraform/gaia/terraform.tf
@@ -1,6 +1,14 @@
 terraform {
   required_version = "~>1.8"
   required_providers {
+    flux = {
+      source  = "fluxcd/flux"
+      version = "~>1.0"
+    }
+    github = {
+      source  = "integrations/github"
+      version = "~>6.0"
+    }
     helm = {
       source  = "hashicorp/helm"
       version = "~>3.0"
@@ -21,7 +29,33 @@ terraform {
       source  = "siderolabs/talos"
       version = "0.9.0-alpha.0"
     }
+    tls = {
+      source = "hashicorp/tls"
+      version = "~>4.0"
+    }
   }
+}
+
+provider "flux" {
+  kubernetes = {
+    host = module.kubernetes.host
+
+    client_certificate     = base64decode(module.kubernetes.client_certificate)
+    client_key             = base64decode(module.kubernetes.client_key)
+    cluster_ca_certificate = base64decode(module.kubernetes.cluster_ca_certificate)
+  }
+  git = {
+    url = "ssh://git@github.com/${var.github_organization}/${var.github_repository}.git"
+    ssh = {
+      username    = "git"
+      private_key = tls_private_key.flux.private_key_pem
+    }
+  }
+}
+
+provider "github" {
+  owner = var.github_organization
+  token = local.github_token
 }
 
 provider "kubernetes" {
--- terraform/gaia/variables.tf
+++ terraform/gaia/variables.tf
@@ -2,3 +2,15 @@ variable "cluster_name" {
   type    = string
   default = "gaia"
 }
+
+variable "github_organization" {
+  description = "Github organization"
+  type        = string
+  default     = "janpuc"
+}
+
+variable "github_repository" {
+  description = "Github repository"
+  type        = string
+  default     = "home-ops"
+}
