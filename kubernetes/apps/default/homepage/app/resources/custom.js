1539fc72d7a8c3cf830d86c7bf9e73ba4120435a
janpuc <janpuc@proton.me>
2025-12-18 09:16:55 +0100

    feat: try to add a service worker for homepage

--- kubernetes/apps/default/homepage/app/resources/custom.js
+++ kubernetes/apps/default/homepage/app/resources/custom.js
@@ -1,1 +1,17 @@
-// Empty
+(() => {
+  if (!('serviceWorker' in navigator)) return;
+
+  window.addEventListener('load', async () => {
+    try {
+      const reg = await navigator.serviceWorker.register('/sw.js', {
+        scope: '/',
+      });
+
+      if (reg.navigationPreload) {
+        await reg.navigationPreload.enable();
+      }
+    } catch (err) {
+      console.error('Service Worker registration failed:', err);
+    }
+  });
+})();
